978419ef287895c07bad3489048d1e203d93f237
Darrenf <darrenf54321@gmail.com>
2016-09-22 12:56:41 +0100

    working code and tests accept both range of nummers and fixed integer

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,25 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "fizzbuzz"
-  elsif number % 3 == 0
-    "fizz"
-  else number % 5 == 0
-    "buzz"
+  if number.is_a?(Range)
+    array = number.to_a
+    array.map!{|num|
+      if (num % 3 == 0) && (num % 5 == 0)
+        "fizzbuzz"
+      elsif num % 3 == 0
+        "fizz"
+      elsif num % 5 == 0
+        "buzz"
+      else num
+      end
+      }
+  else number.is_a?(Fixnum)
+    if (number % 3 == 0) && (number % 5 == 0)
+      "fizzbuzz"
+    elsif number % 3 == 0
+      "fizz"
+    elsif number % 5 == 0
+      "buzz"
+    else number
+    end
   end
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 10' do
     expect(fizzbuzz(10)).to eq 'buzz'
   end
+  it 'returns [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"] when passed 1..15' do
+    expect(fizzbuzz(1..15)).to eq [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"]
+  end
 end
